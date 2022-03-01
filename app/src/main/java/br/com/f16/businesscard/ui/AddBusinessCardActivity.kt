128b93e64961df6951d322dad7f1395dadd13249
Flávio Paixão <flavio_pc@outlook.com>
2022-03-01 10:08:42 -0300

    criação da tela de cadastro dos cartões

---
+++ app/src/main/java/br/com/f16/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,17 @@
+package br.com.f16.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.f16.businesscard.databinding.ActivityAddBusinessCardBinding
+
+
+class AddBusinessCardActivity : AppCompatActivity() {
+    private val binding by lazy{ ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+
+}
