8cac0a6b48809348909ca6d45c94e2bed14f01f8
Flávio Paixão <flavio_pc@outlook.com>
2022-03-01 14:03:38 -0300

    salvar o cartão no banco

--- app/src/main/java/br/com/f16/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/f16/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,11 +2,20 @@ package br.com.f16.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import br.com.f16.businesscard.App
+import br.com.f16.businesscard.R
+import br.com.f16.businesscard.data.BusinessCard
 import br.com.f16.businesscard.databinding.ActivityAddBusinessCardBinding
 
 
 class AddBusinessCardActivity : AppCompatActivity() {
-    private val binding by lazy{ ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,13 +23,24 @@ class AddBusinessCardActivity : AppCompatActivity() {
         insertListeners()
     }
 
- private fun insertListeners(){
-     binding.btClose.setOnClickListener {
-         finish()
-     }
+    private fun insertListeners() {
+        binding.btClose.setOnClickListener {
+            finish()
+        }
 
-     binding.btConfirmar.setOnClickListener {
+        binding.btConfirmar.setOnClickListener {
+            val businessCard = BusinessCard(
+                nome = binding.tilName.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                telefone = binding.tilFone.editText?.text.toString(),
+                fundo = binding.tilCor.editText?.text.toString(),
+            )
 
-     }
- }
+            mainViewModel.insert(businessCard)
+
+            Toast.makeText(this, R.string.txt_sucess,Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
 }
