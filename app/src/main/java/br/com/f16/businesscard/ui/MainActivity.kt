909d89d838e27df97e6f5db1952942b09c03049d
Flávio Paixão <flavio_pc@outlook.com>
2022-03-01 13:24:09 -0300

    criação do MAinViewModel

--- app/src/main/java/br/com/f16/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/f16/businesscard/ui/MainActivity.kt
@@ -3,20 +3,32 @@ package br.com.f16.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import br.com.f16.businesscard.App
 import br.com.f16.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        getAllBusinessCard()
         insertListeners()
     }
 
-    private fun insertListeners(){
-        binding.fabAdd.setOnClickListener{
-            val intent = Intent(this,AddBusinessCardActivity::class.java)
+    private fun insertListeners() {
+        binding.fabAdd.setOnClickListener {
+            val intent = Intent(this, AddBusinessCardActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAllBusinessCard(){
+        mainViewModel.getAll().observe(this,{
+        })
     }
 }
