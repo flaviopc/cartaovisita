3cec94c722c964b0aca9c864128902f66f5acfef
Flávio Paixão <flavio_pc@outlook.com>
2022-03-01 08:32:19 -0300

    commit inicial

---
+++ app/src/main/java/br/com/f16/businesscard/App.kt
@@ -0,0 +1,6 @@
+package br.com.f16.businesscard
+
+import android.app.Application
+
+class App: Application() {
+}
---
+++ app/src/main/java/br/com/f16/businesscard/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.f16.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.f16.businesscard.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
